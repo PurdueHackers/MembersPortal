4bb24c007689cd723c69e4c0fc40d265349ec976
Devin Sova <neoskeeter7@gmail.com>
2018-03-21 19:58:50 -0400

    Updated Event Suggestion Link

--- resources/views/pages/events/eventlist.blade.php
+++ resources/views/pages/events/eventlist.blade.php
@@ -4,7 +4,7 @@
 <div class="section"><div class='section-container'>
 	<h3>Events
 		<a href="{{ action('HackathonController@getIndex') }}" class="pull-left"><button type="button" class="btn btn-info btn-sm marginR">Upcoming Hackathons</button></a>
-		<a href="https://goo.gl/forms/rTv3z2tRk0qRBsYT2" class="pull-right"><button type="button" class="btn btn-primary btn-sm">Submit Event Suggestion</button></a>
+		<a href="https://goo.gl/forms/hAhYgXes2zQeftbR2" class="pull-right"><button type="button" class="btn btn-primary btn-sm">Submit Event Suggestion</button></a>
 		@can ('permission','events')
 		<a href="{{ action('HomeController@getAnvilWifi') }}" class="pull-left"><button type="button" class="btn btn-info btn-sm">Anvil Wifi</button></a>
 		<a href="{{ action('EventController@getCreate') }}" class="pull-right"><button type="button" class="btn btn-primary btn-sm marginR">+ Add Event</button></a>
